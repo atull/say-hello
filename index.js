fa945d021d983e8cf32b01967933a545e1c7b709
atull <atul.limje@gmail.com>
2025-06-25 16:23:59 +0530

    Add GET /user endpoint

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const port = process.env.PORT || 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+// ...existing code...
+
+app.get('/user', (req, res) => {
+  res.send('User endpoint')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
